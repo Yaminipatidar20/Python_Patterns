fec0ee64f41d4354a4b69a54d768214d2eeac36a
Yamini Patidar <yaminipatidar20@gmail.com>
2021-10-20 20:30:35 +0530

    Add files via upload

---
+++ Pattern-10.py
@@ -0,0 +1,12 @@
+# Code -1
+"""n=int(input("Enter the number of rows:-"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print() """
+
+# Code -2
+
+n=int(input("Enter the number of rows:-"))
+for i in range(1,n+1):
+    print(" *"*i)
